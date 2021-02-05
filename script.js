eda10e5842953f8f6c0c51936cacf7ed506ab4b1
Jared Long <jlong4223@gmail.com>
2021-02-04 22:39:32 -0600

    defines app and controller

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // the module name follows angular.module
 let myApp = angular.module("myList", []);
+
+// myApp is now able to grab things from angular module, like below, I am creating an angular controller, its name and a function
+myApp.controller("myListController", function ($scope) {});
