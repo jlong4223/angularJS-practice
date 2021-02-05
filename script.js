278562476baa583b4b4379110f3c8ed3b19c414b
Jared Long <jlong4223@gmail.com>
2021-02-04 22:50:49 -0600

    created items array

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 let myApp = angular.module("myList", []);
 
 // myApp is now able to grab things from angular module, like below, I am creating an angular controller, its name and a function
-myApp.controller("myListController", function ($scope) {});
+myApp.controller("myListController", function ($scope) {
+  // every item binded in this controller can be binded to index.html bc the index.html controller is using this one
+  $scope.items = ["AngularJS", "ReactJS", "VueJS"];
+  $scope.newItem = " ";
+});
