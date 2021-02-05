9a73e2392cfa2093e98b565152cc83f86af90cd8
Jared Long <jlong4223@gmail.com>
2021-02-04 23:03:58 -0600

    adds function for btn

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let myApp = angular.module("myList", []);
 // myApp is now able to grab things from angular module, like below, I am creating an angular controller, its name and a function
 myApp.controller("myListController", function ($scope) {
   // every item binded in this controller can be binded to index.html bc the index.html controller is using this one
+  $scope.title = "Javascript Frameworks";
   $scope.items = ["AngularJS", "ReactJS", "VueJS"];
-  $scope.newItem = " ";
+  $scope.newItem = "";
+
+  $scope.pushItem = function () {
+    console.log("clicked");
+  };
 });
